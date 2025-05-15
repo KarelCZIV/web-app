11a73a3b18f5a38c775780ba48df59d4c1a3d347
KarlCZIV <karlvestec@gmail.com>
2025-05-15 16:10:45 +0200

    Initial commit

---
+++ modules/functions.py
@@ -0,0 +1,17 @@
+FILEPATH = 'C:/Users/TomasCelustka/Desktop/pythonProject/files/todos.txt'
+
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file adn return the list of to-do items """   # doc strings
+    with open(filepath, 'r') as file_def:
+        todos_def = file_def.readlines()
+    return todos_def
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write the to-do items list in the text file """
+    with open(filepath, 'w') as file_def:
+        file_def.writelines(todos_arg)
+
+if __name__ == "__main__":
+    print(get_todos())
